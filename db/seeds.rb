6659bb10ccb28b750bf391de1328135972c8dd93
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-08-26 15:48:37 +0300

    Assignment Complete

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+puts 'Seeding...'
+
+Show.create(name: "TVD", network: "The CW", day: "Thursday", rating: 9, season: "two" )
+Show.create(name: "PLL", network: "CBS", day: "Friday", rating: 8, season: "three" )
+Show.create(name: "A.D", network: "Fox", day: "Monday", rating: 10, season: "one" )
+Show.create(name: "Friends", network: "NBC", day: "Wednesday", rating: 7, season: "ten" )
+Show.create(name: "A.D", network: "HBO", day: "Tuesday", rating: 3, season: "five" )
+
+puts "Done Seeding!"
